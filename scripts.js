50349e0b379aa8d7a8f635179924d96f256bfff9
omartins-zs <gabrielmatheus209@gmail.com>
2024-12-19 09:53:24 -0300

    Comentando as Tarefas Iniciais

--- scripts.js
+++ scripts.js
@@ -1,18 +1,18 @@
-var todos = [
-    {
-        text: "Aprender o básico de Vue JS",
-        done: true,
-    },
-    {
-        text: "Aprender HTML, CSS e Javascript",
-        done: false,
-    },
-];
+// var todos = [
+//     {
+//         text: "Aprender o básico de Vue JS",
+//         done: true,
+//     },
+//     {
+//         text: "Aprender HTML, CSS e Javascript",
+//         done: false,
+//     },
+// ];
 
 const todosApp = {
     data() {
         return {
-            todos: window.todos,
+            todos: [],
             newTodo: {
                 done: false,
             },
